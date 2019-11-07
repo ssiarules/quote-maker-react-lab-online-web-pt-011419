23a3f5ea90d33c1f1184fbc22a3f39cf03c8e723
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-07 16:51:27 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,9 @@
 // TODO: Create action creators as defined in tests
+
+const addQuote = (quote) => {
+  return {type: "ADD_QUOTE", quote: {...quote, votes: 0}}
+}
+
+const removeQuote = quoteID => {
+  return {type: REMOVE_QUOTE}, quoteID}
+}
